c10782d14987efd8fa4c0c6953b02d1219815c6f
sota20040308 <2201124@s.asojuku.ac.jp>
2024-01-11 10:41:50 +0900

    1

---
+++ src/select.php
@@ -0,0 +1,8 @@
+<?php 
+    const SERVER = 'mysql215.phy.lolipop.lan';
+    const DBNAME = 'LAA1517457-final';
+    const USER = 'LAA1517457';
+    const PASS = 'Pass0308';
+
+    $connect = 'mysql:host='. SERVER . ';dbname='. DBNAME . ';charset=utf8';
+?>
