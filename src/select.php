3ca45a02937bc7b602237334d9611b5113884ade
sota20040308 <2201124@s.asojuku.ac.jp>
2024-01-11 10:47:43 +0900

    1

--- src/select.php
+++ src/select.php
@@ -6,3 +6,16 @@
 
     $connect = 'mysql:host='. SERVER . ';dbname='. DBNAME . ';charset=utf8';
 ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title></title>
+    <link rel="stylesheet" href="./css/style.css">
+</head>
+<body>
+<h2>選手一覧</h2>
+<a href="index.php">メニューに戻る</a>
+<hr>
+</html>
