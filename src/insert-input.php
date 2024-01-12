158224f1f4d3ca08d9aea8649051f4053e05af04
sota20040308 <2201124@s.asojuku.ac.jp>
2024-01-12 14:58:31 +0900

    1

--- src/insert-input.php
+++ src/insert-input.php
@@ -1,3 +1,11 @@
+<?php 
+    const SERVER = 'mysql220.phy.lolipop.lan';
+    const DBNAME = 'LAA1517457-final';
+    const USER = 'LAA1517457';
+    const PASS = 'Pass0308';
+
+    $connect = 'mysql:host='. SERVER . ';dbname='. DBNAME . ';charset=utf8';
+?>
 <!DOCTYPE html>
 <html lang="ja">
     <head>
