ba153600fd3667c97d14954488ab05e1d860723b
sota20040308 <2201124@s.asojuku.ac.jp>
2024-01-12 12:13:47 +0900

    1

--- src/select.php
+++ src/select.php
@@ -2,23 +2,20 @@
 <html lang="ja">
     <head>
         <meta charset="UTF-8">
-        <title>課題6-3-2</title>
+        <title>選手一覧</title>
     </head>
     <body>
         <table>
             <tr><th>番号</th><th>選手名</th><th>チーム名</th></tr>
             <?php
-            $pdo=new PDO('mysql:host=mysql212.phy.lolipop.lan;dbname=LAA1517480-sample;charset=utf8',
-            username:'LAA1517480',
-            password:'Pass0101');
-            foreach($pdo->query('select * from Shohin')as $row){
+            $pdo=new PDO('mysql:host=mysql220.phy.lolipop.lan;dbname=LAA1517457-final;charset=utf8',
+            username:'LAA1517457',
+            password:'Pass0308');
+            foreach($pdo->query('select * soccerplayer from Shohin')as $row){
                 echo '<tr>';
-                echo '<td>',$row['shohin_id'],'</td>';
-                echo '<td>',$row['shohin_mei'],'</td>';
-                echo '<td>',$row['shohin_bunrui'],'</td>';
-                echo '<td>',$row['hanbai_tanka'],'</td>';
-                echo '<td>',$row['shiire_tanka'],'</td>';
-                echo '<td>',$row['torokubi'],'</td>';
+                echo '<td>',$row['id'],'</td>';
+                echo '<td>',$row['name'],'</td>';
+                echo '<td>',$row['team'],'</td>';
                 echo '</tr>';
                 echo "\n";
             }
