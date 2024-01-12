b1f05774d2d3c15ba908299ad6912202b0cd7919
sota20040308 <2201124@s.asojuku.ac.jp>
2024-01-12 12:22:31 +0900

    1

--- src/index.php
+++ src/index.php
@@ -1,11 +1,3 @@
-<?php 
-    const SERVER = 'mysql215.phy.lolipop.lan';
-    const DBNAME = 'LAA1517457-final';
-    const USER = 'LAA1517457';
-    const PASS = 'Pass0308';
-
-    $connect = 'mysql:host='. SERVER . ';dbname='. DBNAME . ';charset=utf8';
-?>
 <!DOCTYPE html>
 <html>
 <head>
