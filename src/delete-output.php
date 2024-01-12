1472ad27be99d488fc4b9e04ade39c2927d09a9c
sota20040308 <2201124@s.asojuku.ac.jp>
2024-01-12 14:35:57 +0900

    1

---
+++ src/delete-output.php
@@ -0,0 +1,44 @@
+<?php 
+    const SERVER = 'mysql220.phy.lolipop.lan';
+    const DBNAME = 'LAA1517457-final';
+    const USER = 'LAA1517457';
+    const PASS = 'Pass0308';
+
+    $connect = 'mysql:host='. SERVER . ';dbname='. DBNAME . ';charset=utf8';
+?>
+<!DOCTYPE html>
+<html lang="ja">
+	<head>
+		<meta charset="UTF-8">
+		<title></title>
+	</head>
+	<body>
+<?php
+    $pdo=new PDO($connect, USER, PASS);
+    $sql=$pdo->prepare('delete from soccerplayer where id=?');
+    if ($sql->execute([$_GET['id']])){
+        echo '削除に成功しました。';
+    }else{
+        echo '削除に失敗しました。';
+    }
+
+?>
+    <br><hr><br>
+	<table>
+    <tr><th>番号</th><th>選手名</th><th>チーム名</th></tr>
+<?php
+    foreach ($pdo->query('select * from soccerplayer') as $row) {
+        echo '<tr>';
+        echo '<td>',$row['id'], '</td>';
+        echo '<td>',$row['name'], '</td>';
+        echo '<td>',$row['team'], '</td>';
+        echo '</tr>';
+        echo "\n";
+    }
+?> 
+</table>
+    <form action="index.php" method="post">
+        <button type="submit">削除画面へ戻る</button>
+    </form>
+    </body>
+</html>
