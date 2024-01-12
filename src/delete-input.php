91c47b27adf5114263f5456fc2cd2d393d76c5a5
sota20040308 <2201124@s.asojuku.ac.jp>
2024-01-12 14:34:34 +0900

    1

--- src/delete-input.php
+++ src/delete-input.php
@@ -13,31 +13,24 @@
 		<title></title>
 	</head>
 	<body>
-    <h2>選手削除</h2>
-<a href="index.php">メニューに戻る</a>
+        <table>
+    <tr><th>番号</th><th>選手名</th><th>チーム名</th></tr>
 <?php
     $pdo=new PDO($connect, USER, PASS);
-    $sql=$pdo->prepare('delete from soccerplayer where id=?');
-    if ($sql->execute([$_POST['id']])){
-        echo '削除に成功しました。';
-    }else{
-        echo '削除に失敗しました。';
-    }
-
-?>
-    <br><hr><br>
-	<table>
-		<tr><th>番号</th><th>選手名</th><th>チーム名</th></tr>
-<?php
-    foreach ($pdo->query('select * from soccerplayer ') as $row) {
+    foreach($pdo->query('select * from soccerplayer') as $row){
         echo '<tr>';
         echo '<td>',$row['id'], '</td>';
         echo '<td>',$row['name'], '</td>';
         echo '<td>',$row['team'], '</td>';
+        echo '<td>';
+        echo '<a href="delete-output.php?id=', $row['id'], '">削除</a>';
+        echo '</td>';
         echo '</tr>';
         echo "\n";
     }
-?> 
-</table>
-</body>
+
+?>
+    </table>
+    </body>
 </html>
+
